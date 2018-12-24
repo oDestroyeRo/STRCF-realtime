f8f4c163dc85786eace705063d766efb12362658
Dos <natpakan.w@ku.th>
2018-12-24 14:37:51 +0800

    fix some bug

--- utils/create_realtime_sequence.m
+++ utils/create_realtime_sequence.m
@@ -1,10 +1,9 @@
-function [params] = create_realtime_sequence(img,bbox,img_real)
+function [params] = create_realtime_sequence(img,bbox)
 
     seq.format = 'realtime';
     seq.init_rect = bbox;
     seq.img = img;
     seq.num_frames = 1;
-    seq.img_real = img_real;
     seq.isFirst = false;
     seq.isOldPos = false;
     params.seq = seq;
