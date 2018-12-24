a7dc771e2a522d9aa1f42a927ae3c8a02be804df
Dos <natpakan.w@ku.th>
2018-12-24 14:18:58 +0800

    fix some bug

--- utils/create_realtime_sequence.m
+++ utils/create_realtime_sequence.m
@@ -1,10 +1,13 @@
-function [seq] = create_realtime_sequence(img,bbox,img_real)
+function [params] = create_realtime_sequence(img,bbox,img_real)
 
-seq.format = 'realtime';
-seq.init_rect = bbox;
-seq.img = img;
-seq.num_frames = 1;
-seq.img_real = img_real;
+    seq.format = 'realtime';
+    seq.init_rect = bbox;
+    seq.img = img;
+    seq.num_frames = 1;
+    seq.img_real = img_real;
+    seq.isFirst = false;
+    seq.isOldPos = false;
+    params.seq = seq;
 
 end
 
